790d7dfd629ade46252133509810a85b8bed052b
zihuatanejp <aimilixi@gmail.com>
2018-09-17 14:11:10 +0800

    re deploy

--- comps/layout.js
+++ comps/layout.js
@@ -115,6 +115,7 @@ class Layout extends React.Component {
                     <script type="text/javascript" src="/static/lightwallet.min.js" ></script>
                     <script src="/static/web3.min.js"></script>
                     <script src="/static/eth-wallet-js.js"></script>
+                    <base href="https://truechain.github.io/webwallet/" />
                 </Head>
                 <div className='header-wrapper'>
                     <div className='header'>
