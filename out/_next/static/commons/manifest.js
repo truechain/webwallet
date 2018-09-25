06d18e7b52c68b77ae008d3d0e36f50d1351e020
zihuatanejp <aimilixi@gmail.com>
2018-09-25 15:49:30 +0800

    redeployed

--- out/_next/static/commons/manifest.js
+++ out/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "138ec64f15efc75bfdf8"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "dc9460b94729a8020744"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_app.js","2":"bundles\\pages\\_document.js","3":"bundles\\pages\\_error.js","5":"bundles\\pages\\index.js","6":"bundles\\pages\\transfer.js","8":"bundles\\pages\\check-account.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_app.js","2":"bundles\\pages\\_document.js","3":"bundles\\pages\\_error.js","5":"bundles\\pages\\index.js","6":"bundles\\pages\\transfer.js","7":"bundles\\pages\\check-tx.js","8":"bundles\\pages\\check-account.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
