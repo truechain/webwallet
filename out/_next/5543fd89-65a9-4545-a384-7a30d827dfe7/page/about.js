1cb99b28953c78bc3dfddbe8bccb3761a489227f
zihuatanejp <aimilixi@gmail.com>
2018-11-13 11:57:12 +0800

    redeployed

--- out/_next/5543fd89-65a9-4545-a384-7a30d827dfe7/page/about.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:webpackJsonp([7],{402:function(e,t,n){e.exports=n(403)},403:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(1),a=n.n(u);t.default=function(){return a.a.createElement("div",null,a.a.createElement("p",null,"This is the about page"))}}},[402]).default}});
