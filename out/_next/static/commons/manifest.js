2b561cdfe9c3a1e06f5be48cec410fd9b0d1ea1a
zihuatanejp <aimilixi@gmail.com>
2018-12-13 09:54:10 +0800

    fix privatekey import and redeployed

--- out/_next/static/commons/manifest.js
+++ out/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "8bd065573f94e5f9ea9d"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "eaa8de31c3926c75d742"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
