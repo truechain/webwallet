6b5326233f5641ae48836f88746018d0674522bf
zihuatanejp <aimilixi@gmail.com>
2019-03-18 16:49:51 +0800

    update to newest

--- out/_next/14ffc359-dd76-4623-a442-12f2008cfc13/page/about.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:webpackJsonp([7],{405:function(e,t,n){e.exports=n(406)},406:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(1),a=n.n(u);t.default=function(){return a.a.createElement("div",null,a.a.createElement("p",null,"This is the about page"))}}},[405]).default}});
